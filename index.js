221132384f3b04dc58d0ceb623fbb724a90a61ef
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-31 21:33:34 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000');
-})
-
+app.get('/dogs', function(request, response) {
+    return response.send('Dogs go brk brk');
+  });
+  
+  app.listen(3000, function(){
+    console.log('App on port 3000');
+  })
 //touch index.js sets up the file right away in terminal 
