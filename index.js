2b7b3f7f14056e66f235e130b1f62fbdc6816c9f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-31 21:36:01 -0400

    update

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.get('/dogs', function(request, response) {
     console.log('App on port 3000');
   })
 //touch index.js sets up the file right away in terminal 
+
+
+// psql Common Commands
+// \l — List all databases
+// \c DB_NAME — connect to DB_NAME
+// \dt —- List all tables (in current db)
+// \d TABLE_NAME — Get details about TABLE_NAME (in current db)
+// \q — Quit psql (can also type <Control-D>)
